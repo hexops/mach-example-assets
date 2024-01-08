4beb172383b0012b256a26c59bf7bb97426436b8
Ali Chraghi <alichraghi@proton.me>
2024-01-08 19:43:19 +0330

    update to latest build API

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
 
     _ = b.addModule("mach-core-example-assets", .{
-        .source_file = .{ .path = "main.zig" },
+        .root_source_file = .{ .path = "main.zig" },
     });
 
     const main_tests = b.addTest(.{
